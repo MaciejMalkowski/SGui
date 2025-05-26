138e8d664a76350737c797b06ba5692e9d171c02
Praktykant <praktykant@marbotechnology.pl>
2025-05-26 10:18:16 +0200

    c webserver

--- server.c
+++ server.c
@@ -1,30 +1,31 @@
-#include <WinSock2.h>
+#include <winsock2.h>
 #include <Windows.h>
 #include <stdio.h>
+#include <ws2tcpip.h>
 
 int main()
 {
-    WSDATA wsdata;
-    WSAStartup(MAKEWORD(2, 2), &wsdata);
-    scoket(AF_INET, SOCK_STREAM, 0);
-    socket S = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = 0;
-    addr.sin_port = htons(8080);
-    bind(s, &addr, sizeof(addr));
+	WSADATA wsadata;
+	WSAStartup(MAKEWORD(2, 2), &wsadata);
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = 0;
+	addr.sin_port = htons(8080);
+	bind(s, (struct sockaddr *)&addr, sizeof(addr));
 
-    listen(s ,10);
+	listen(s, 10);
 
-    SOCKET client = accept(s, 0, 0);
-    char request[256] = {0};
-    recv(client, request, 256, 0);
-    // GET /[file] ...
-    if (memcmp(request, "GET / ", 6) == 0){
-        FILE* f = fopen("index.html", "r");
-        char buffer[256] = {0};
-        fread(buffer, 1, 256, f);
-        send(client, buffer, 256, 0);
+	SOCKET client = accept(s, 0, 0);
 
-    }
+	char request[256] = {0};
+	recv(client, request, 256, 0);
+
+	// GET /[file] ...
+	if (memcmp(request, "GET / ", 6) == 0) {
+		FILE* f = fopen("index.html", "r");
+		char buffer[256] = {0};
+		fread(buffer, 1, 256, f);
+		send(client, buffer, 256, 0);
+	}
 }
